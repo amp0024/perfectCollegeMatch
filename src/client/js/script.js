3665db730872e68d4c752a51d3438c38647283e9
amp0024 <amp0024@gmail.com>
2016-03-15 13:35:20 -0600

    simple map created

--- src/client/js/script.js
+++ src/client/js/script.js
@@ -1,67 +1,82 @@
-(function(window, mapster) {
+var locations = [
+    ['Test', '10261 Macedonia St, CO', 'Location 1 URL'],
+    ['Location 2 Name', 'Newark, NJ', 'Location 2 URL'],
+    ['Location 3 Name', 'Philadelphia, PA', 'Location 3 URL']
+];
 
-	// map options
-	var options = mapster.MAP_OPTIONS,
-	element = document.getElementById('map-canvas'),
-	// map
-	map = mapster.create(element, options);
+var geocoder;
+var map;
+var bounds = new google.maps.LatLngBounds();
 
-	// map._on('click', function(e){
-	// 	alert('click');
-	// 	console.log(e);
-	// 	console.log(this);
-	// });
-	
-	// var marker = map.addMarker({
-	// 	lat: 39.7392,
-	// 	lng: 104.9903,
-	// 	visible: true,
-	// 	id: 1,
-	// 	content: '<div style="color: red;">I like food</div>'
-	// });
+function initialize() {
+    map = new google.maps.Map(
+    document.getElementById("map_canvas"), {
+        center: new google.maps.LatLng(37.4419, -122.1419),
+        zoom: 13,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
+    geocoder = new google.maps.Geocoder();
 
-	var marker2 = map.addMarker({
-		lat: 40,
-		lng: 104.9903,
-		visible: true,
-		id: 2,
-		content: '<div style="color: red;">I like food</div>'
-	});
-
-	for (var i = 0; i < 40; i++){
-		map.addMarker({
-			lat: 40 + Math.random(),
-			lng: 104.9903 + Math.random(),
-			visible: true,
-			id: 2,
-			content: '<div style="color: red;">I like food</div>'
-		});
-
-		var marker = map.addMarker({
-			lat: 40 + Math.random(),
-			lng: 104.9903 + Math.random(),
-			visible: true,
-			content: '<div style="color: red;">I like food</div>'
-		});
-	}
-	// var found = map.findby(function(marker){
-	// 	return marker.id === 2;
-	// });
-
-	map.removeBy(function(marker){
-		if (marker.id === 2){
-			console.log(marker);
-		}
-		return marker.id === 2;
-	});
+    for (i = 0; i < locations.length; i++) {
 
 
-	// var marker = new google.maps.Marker({
-	// 	position: {
-	// 		lat: 39.7392,
-	// 		lng: 104.9903
-	// 	},
-	// 	map: map.gMap
-	// });
+        geocodeAddress(locations, i);
+    }
+}
+google.maps.event.addDomListener(window, "load", initialize);
 
-}(window, window.Mapster));
+function geocodeAddress(locations, i) {
+    var title = locations[i][0];
+    var address = locations[i][1];
+    var url = locations[i][2];
+    geocoder.geocode({
+        'address': locations[i][1]
+    },
+
+    function (results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+            var marker = new google.maps.Marker({
+                icon: 'http://maps.google.com/mapfiles/ms/icons/blue.png',
+                map: map,
+                position: results[0].geometry.location,
+                title: title,
+                animation: google.maps.Animation.DROP,
+                address: address,
+                url: url
+            })
+            infoWindow(marker, map, title, address, url);
+            bounds.extend(marker.getPosition());
+            map.fitBounds(bounds);
+        } else {
+            alert("geocode of " + address + " failed:" + status);
+        }
+    });
+}
+
+function infoWindow(marker, map, title, address, url) {
+    google.maps.event.addListener(marker, 'click', function () {
+        var html = "<div><h3>" + title + "</h3><p>" + address + "<br></div><a href='" + url + "'>View location</a></p></div>";
+        iw = new google.maps.InfoWindow({
+            content: html,
+            maxWidth: 350
+        });
+        iw.open(map, marker);
+    });
+}
+
+function createMarker(results) {
+    var marker = new google.maps.Marker({
+        icon: 'http://maps.google.com/mapfiles/ms/icons/blue.png',
+        map: map,
+        position: results[0].geometry.location,
+        title: title,
+        animation: google.maps.Animation.DROP,
+        address: address,
+        url: url
+    })
+    bounds.extend(marker.getPosition());
+    map.fitBounds(bounds);
+    infoWindow(marker, map, title, address, url);
+    return marker;
+}
+
